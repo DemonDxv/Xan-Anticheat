3e3371ff96d66450aee51c2ade8de3656afe645e
DemonDxv <55443927+DemonDxv@users.noreply.github.com>
2020-09-06 15:28:27 -0500

    The Beginning

---
+++ src/dev/demon/xan/Xan.java
@@ -0,0 +1,21 @@
+package dev.demon.xan;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Xan extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+
+        super.onEnable();
+    }
+
+
+    @Override
+    public void onDisable() {
+
+        super.onDisable();
+    }
+
+}
